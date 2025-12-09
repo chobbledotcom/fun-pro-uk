7c9d4ebd92a049add87699061e61d8e5d1048f4a
Stefan <git@stefn.co.uk>
2025-12-09 17:14:05 +0000

    Font fixes

--- scripts/watch.js
+++ scripts/watch.js
@@ -1,13 +1,16 @@
-const fs = require('fs');
-const path = require('path');
-const { sync } = require('./prepare-dev');
+const fs = require("fs");
+const path = require("path");
+const { sync } = require("./prepare-dev");
 
-const root = path.resolve(__dirname, '..');
+const root = path.resolve(__dirname, "..");
 
 fs.watch(root, { recursive: true }, (event, file) => {
-  if (file && (file.endsWith('.md') || file.endsWith('.scss') || file.endsWith('.json'))) {
-    if (!file.startsWith('.build') && !file.startsWith('node_modules') && !file.startsWith('.git')) {
-      sync();
-    }
-  }
+	if (
+		file &&
+		!file.startsWith(".build") &&
+		!file.startsWith("node_modules") &&
+		!file.startsWith(".git")
+	) {
+		sync();
+	}
 });
