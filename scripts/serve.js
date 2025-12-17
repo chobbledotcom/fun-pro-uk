4519b3378791bf26618f49ad83d4eb1aab80b4d1
Stefan <git@stefn.co.uk>
2025-12-17 17:33:42 +0000

    Theme fixes

--- scripts/serve.js
+++ scripts/serve.js
@@ -1,10 +1,22 @@
-const { spawn } = require('child_process');
+const { spawn, spawnSync } = require('child_process');
 const path = require('path');
 const { prep } = require('./prepare-dev');
 
 const dev = path.join(__dirname, '..', '.build', 'dev');
 
 prep();
+
+console.log('Installing dependencies...');
+const install = spawnSync('pnpm', ['install'], {
+  cwd: dev,
+  stdio: 'inherit',
+  shell: true
+});
+
+if (install.status !== 0) {
+  console.error('Failed to install dependencies');
+  process.exit(1);
+}
 
 console.log('Starting server...');
 
