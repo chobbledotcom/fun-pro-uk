4c12ca0e62b6e0c6a812d91027509adb60fea8ff
Stefan <git@stefn.co.uk>
2026-02-02 15:48:27 +0000

    Fix nav-bottom

--- assets/js/nav.js
+++ assets/js/nav.js
@@ -1,19 +1,19 @@
-(function () {
-  function updateNavPosition() {
-    const nav = document.querySelector("nav ul");
-    const rect = nav.getBoundingClientRect();
-    document.documentElement.style.setProperty(
-      "--nav-bottom",
-      rect.bottom - 5 + "px",
-    );
-  }
+(() => {
+	function updateNavPosition() {
+		const nav = document.querySelector("nav ul");
+		const rect = nav.getBoundingClientRect();
+		document.documentElement.style.setProperty(
+			"--nav-bottom",
+			rect.bottom - 5 + "px",
+		);
+	}
 
-  if (document.readyState === "loading") {
-    document.addEventListener("DOMContentLoaded", updateNavPosition);
-  } else {
-    updateNavPosition();
-  }
+	if (document.readyState === "loading") {
+		document.addEventListener("DOMContentLoaded", updateNavPosition);
+	} else {
+		updateNavPosition();
+	}
 
-  window.addEventListener("resize", updateNavPosition);
-  window.addEventListener("scroll", updateNavPosition);
+	window.addEventListener("resize", updateNavPosition);
+	window.addEventListener("scroll", updateNavPosition);
 })();
